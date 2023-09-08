de2e0f915457ae03fca6b601808e5b018e7b60f3
Иван <ivanhlop@gmail.com>
2023-09-08 19:07:05 +0300

    Done

--- src/main/java/pro/sky/skyprocalculator/CalculatorServiceImpl.java
+++ src/main/java/pro/sky/skyprocalculator/CalculatorServiceImpl.java
@@ -9,35 +9,35 @@ public class CalculatorServiceImpl {
     public String hello(){
         return "Добро пожаловать в калькулятор";
     }
-    public String sum(Integer a, Integer b){
+    public int sum(Integer a, Integer b){
         if (a != null || b !=null){
-        return a + " + " + b + " = "+ (a + b);
+        return a + b;
     } else {
             throw new IllegalArgumentException("Отстутвуют входные данные");
         }
     }
-    public String minus(Integer a, Integer b){
+    public int minus(Integer a, Integer b){
         if (a != null || b !=null){
-        return a + " - " + b + " = "+ (a - b);
+        return a - b;
     }
         else {
             throw new IllegalArgumentException("Отстутвуют входные данные");
         }
     }
-    public String multiply(Integer a, Integer b) {
+    public int multiply(Integer a, Integer b) {
         if (a != null || b != null) {
-            return a + " * " + b + " = " + (a * b);
+            return a * b;
         }
         else{
                 throw new IllegalArgumentException("Отстутвуют входные данные");
             }
         }
-        public String divide (Integer a, Integer b){
+        public int divide (Integer a, Integer b){
         if (b == 0){
             throw new IllegalArgumentException("Делитель не может быть равен нулю");
         }
             if (a != null || b != null) {
-                return a + " / " + b + " = " + (a / b);
+                return a / b;
             }
             else{
                     throw new IllegalArgumentException("Отстутвуют входные данные ");
